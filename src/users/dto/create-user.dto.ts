b4944961c387b7ff151b9e2ac9994a789d0c1a6f
NaokiHaba <naosprintrunner6385@gmail.com>
2023-02-05 02:30:57 +0900

    feat: DTOを定義

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+import { IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @MaxLength(255)
+  @IsNotEmpty()
+  name: string;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @MaxLength(255)
+  @IsNotEmpty()
+  name: string;
+}
