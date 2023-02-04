7001eede2b01e5bd24c67e0a8659f28a34b36df2
NaokiHaba <naosprintrunner6385@gmail.com>
2023-02-05 02:34:26 +0900

    feat: ユーザーを作成するサービスを実装

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
-
   beforeEach(async () => {
+    // テスト用のモジュールを作成する
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
+    // テスト用のモジュールから、UsersServiceを取得する
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  // テストケース
+  describe('create()', () => {
+    it('should successfully insert a user', () => {
+      const dto: CreateUserDto = {
+        name: '太郎',
+      };
+
+      jest
+        .spyOn(service, 'create')
+        .mockImplementation(async (dto: CreateUserDto) => {
+          const user: User = {
+            id: 1,
+            ...dto,
+          };
+          return user;
+        });
+
+      expect(service.create(dto)).resolves.toEqual({
+        id: 1,
+        ...dto,
+      });
+    });
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create({ name }: CreateUserDto): Promise<User> {
+    return await this.userRepository
+      .save({
+        name: name,
+      })
+      .catch((e) => {
+        throw new InternalServerErrorException(
+          `[${e.message}]：ユーザーの登録に失敗しました。`,
+        );
+      });
   }
 
   findAll() {
