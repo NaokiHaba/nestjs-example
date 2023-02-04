5a0150a7320b08f458c0e6cad0ddcb4737d7eb3b
NaokiHaba <naosprintrunner6385@gmail.com>
2023-02-05 02:19:29 +0900

    feat: TypeORMを依存注入

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    AppModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: ['dist/**/entities/**/*.entity.js'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
