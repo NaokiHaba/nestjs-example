4e2391ac725d466745672ce65abea59117314917
NaokiHaba <naosprintrunner6385@gmail.com>
2023-02-05 02:18:55 +0900

    feat: Entity定義

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn({
+    comment: 'アカウントID',
+  })
+  readonly id: number;
+
+  @Column('varchar', { comment: 'アカウント名' })
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
